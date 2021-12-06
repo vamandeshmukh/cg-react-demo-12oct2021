9e2dfec1459113c4f6c8e82117310b508c0b3a2c
vamandeshmukh <vamandeshmukh@gmail.com>
2021-12-06 12:29:52 +0530

    C

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>CG React App</h1>
+      <p>Welcome to Capgemini React App...</p>
+      <p>Welcome to Capgemini React App...</p>
+      <p>Welcome to Capgemini React App...</p>
+      <p>Welcome to Capgemini React App...</p>
+      <p>Welcome to Capgemini React App...</p>
+      <p>Welcome to Capgemini React App...</p>
+      <p>Welcome to Capgemini React App...</p>
+      <p>Welcome to Capgemini React App...</p>
     </div>
   );
 }
-
 export default App;
+
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+// export default App;
