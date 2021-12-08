5e28df1c6fd8134120bf180cf973e093c00d808b
vamandeshmukh <vamandeshmukh@gmail.com>
2021-12-08 15:06:51 +0530

    A

--- src/App.js
+++ src/App.js
@@ -7,57 +7,68 @@ import Hello from "./components/Hello";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import SpringBootData from "./components/SpringBootData";
+import Routes from "./Routes";
 
 const App = () => {
-  console.log('Parent component');
-  const num = 10;
-  const myData = { abc: 15, def: 25 }
-  const myArray = [10, 20, 30];
-
-  const [dataFromChild, setDataFromChild] = useState(4);
-
-  useEffect(() => {
-    console.log('parent useEffect');
-    setDataFromChild(5);
-  }, []);
-
-  const parentFun = (arg) => {
-    console.log('parentFun');
-    setDataFromChild(arg()); // callback
-    console.log(dataFromChild);
-  }
-
-
   return (
     <div>
-      <Header />
-      <div className="container">
-        <p className="display-4 text-primary" >CG React App</p>
-        <SpringBootData />
-        {/* <Hello /> */}
-        {/* <Login /> */}
-        {/* <Register /> */}
-        {/* <p>Child data in parent component: {dataFromChild} </p> */}
-        {/* This will not work */}
-        {/* <p>{myData} </p> */}
-        {/* This will work  */}
-        {/* <p>{myData.abc} </p>
-        <p>{myData.def} </p>
-        <p>{myArray}</p>
-        <p>{myArray[0]}</p>
-        <p>{num}</p>
-        <EmpData
-          // normalFun={aNormalFunction}
-          callParentFun={parentFun}
-          someData={myData}
-          numToPassToChild={num}
-          anotherData={myData}
-        /> */}
-      </div>
-    </div>
+      <Routes />
+
+    </ div>
   );
 }
 export default App;
+
+
+//   console.log('Parent component');
+//   const num = 10;
+//   const myData = { abc: 15, def: 25 }
+//   const myArray = [10, 20, 30];
+
+//   const [dataFromChild, setDataFromChild] = useState(4);
+
+//   useEffect(() => {
+//     console.log('parent useEffect');
+//     setDataFromChild(5);
+//   }, []);
+
+//   const parentFun = (arg) => {
+//     console.log('parentFun');
+//     setDataFromChild(arg()); // callback
+//     console.log(dataFromChild);
+//   }
+
+
+//   return (
+//     <div>
+//       <Header />
+//       <div className="container">
+//         <p className="display-4 text-primary" >CG React App</p>
+//         <SpringBootData />
+//         {/* <Hello /> */}
+//         {/* <Login /> */}
+//         {/* <Register /> */}
+//         {/* <p>Child data in parent component: {dataFromChild} </p> */}
+//         {/* This will not work */}
+//         {/* <p>{myData} </p> */}
+//         {/* This will work  */}
+//         {/* <p>{myData.abc} </p>
+//         <p>{myData.def} </p>
+//         <p>{myArray}</p>
+//         <p>{myArray[0]}</p>
+//         <p>{num}</p>
+//         <EmpData
+//           // normalFun={aNormalFunction}
+//           callParentFun={parentFun}
+//           someData={myData}
+//           numToPassToChild={num}
+//           anotherData={myData}
+//         /> */}
+//       </div>
+//     </div>
+//   );
+// }
+// export default App;
 
 // function App() {
 //   return (
---
+++ src/Routes.js
@@ -0,0 +1,32 @@
+import { Switch as Route, Router /*, Switch */ } from "react-router-dom";
+import EmpData from "./components/EmpData";
+import Header from "./components/Header";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import SpringBootData from "./components/SpringBootData";
+
+const Routes = () => {
+
+    return (
+        <div>
+            <Router>
+                <div>
+                    <Header />
+                    <div>
+                        <Switch>
+                            {/* <Route path='/home' element={<Home/>} /> */}
+                            {/* <Route path="home"> <Home /> </Route> */}
+                            <Route path="/login" element={<Login />} />
+                            <Route path="/register" element={<Register />} />
+                            <Route path="/empdata" element={<EmpData />} />
+                            <Route path="/spring" element={<SpringBootData />} />
+                        </ Switch>
+                    </div>
+                </div>
+            </Router>
+        </div>
+    );
+
+}
+
+export default Routes;
