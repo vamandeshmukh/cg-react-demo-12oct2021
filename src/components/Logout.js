e3ad8f5cd452db794388f462e4cb89ca89b23a60
vamandeshmukh <vamandeshmukh@gmail.com>
2021-12-09 09:10:34 +0530

    Update Logout.js

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,34 +1,48 @@
 const Logout = () => {
 
-    // Source: https://getbootstrap.com/docs/4.0/components/modal/
 
-    // incomplete 
 
     return (
         <div className="mt-3 mb-3">
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-                Logout
-            </button>
-            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title text-primary" id="exampleModalLabel">Logout</h5>
-                            <button type="button" class="close" data-dismiss="modal">
-                                <span>&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            Are you sure you want to logout?
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-success" data-dismiss="modal">Logout</button>
-                            <button type="button" class="btn btn-primary">Stay logged in</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <h1 className="display-4 text-primary" >Logout Component</h1>
+
+
         </div>
     );
 }
 export default Logout;
+
+// const Logout = () => {
+
+//     // Source: https://getbootstrap.com/docs/4.0/components/modal/
+
+//     // incomplete   
+
+//     return (
+//         <div className="mt-3 mb-3">
+//             <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+//                 Logout
+//             </button>
+//             <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog">
+//                 <div class="modal-dialog" role="document">
+//                     <div class="modal-content">
+//                         <div class="modal-header">
+//                             <h5 class="modal-title text-primary" id="exampleModalLabel">Logout</h5>
+//                             <button type="button" class="close" data-dismiss="modal">
+//                                 <span>&times;</span>
+//                             </button>
+//                         </div>
+//                         <div class="modal-body">
+//                             Are you sure you want to logout?
+//                         </div>
+//                         <div class="modal-footer">
+//                             <button type="button" class="btn btn-success" data-dismiss="modal">Logout</button>
+//                             <button type="button" class="btn btn-primary">Stay logged in</button>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
+// export default Logout;
