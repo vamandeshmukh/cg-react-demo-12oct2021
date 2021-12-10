4df281141e477dd975c296b61249b1e4bc6fa0d1
vamandeshmukh <vamandeshmukh@gmail.com>
2021-12-10 10:54:25 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,31 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { act } from 'react-dom/test-utils';
+import Hello from './components/Hello';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+// act(); 
+
+// positive test case 
+test('render Data from backend', () => {
+  render(<Hello />);
+  const linkElement = screen.getByText(/Data from backend/);
   expect(linkElement).toBeInTheDocument();
 });
+
+// // negative test case 
+// test('not to render wrong data', () => {
+//   render(<Hello />);
+//   const linkElement = screen.getByText(/Capgemini is a good company./);
+//   expect(linkElement).not.toBeInTheDocument();
+// });
+
+
+// test('a string value that describes this test', () => { });
+
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
