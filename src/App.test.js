2868f530ba1fb8446d98c4974ba826bd0a93ba52
vamandeshmukh <vamandeshmukh@gmail.com>
2021-12-10 12:30:01 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,9 @@
 import { render, screen } from '@testing-library/react';
-// import { act } from 'react-dom/test-utils';
 import Hello from './components/Hello';
 import EmpData from './components/EmpData';
 import SpringBootData from './components/SpringBootData';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-
 
 // Website https://jestjs.io/ 
 // learning resources https://jestjs.io/docs/getting-started  
@@ -40,12 +38,15 @@ test('render Data from SpringBootData', () => {
 });
 
 
-// // negative test case 
-// test('not to render wrong data', () => {
-//   render(<Hello />);
-//   const linkElement = screen.getByText(/Capgemini is a good company./);
-//   expect(linkElement).not.toBeInTheDocument();
-// });
+// negative test case 
+test('render Data from SpringBootData', () => {
+  render(
+    <Provider store={store} >
+      <SpringBootData />
+    </Provider>);
+  const linkElement = screen.findByText();
+  expect(linkElement).not.toBe('Some other text which is not present in the component.');
+});
 
 
 // test('a string value that describes this test', () => { });
