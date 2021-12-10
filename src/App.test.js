c7448488831f1258330b8a86e67c4b97940a77be
vamandeshmukh <vamandeshmukh@gmail.com>
2021-12-10 12:47:04 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -9,6 +9,33 @@ import store from './redux/store';
 // learning resources https://jestjs.io/docs/getting-started  
 //api https://jestjs.io/docs/api  
 
+// run - 
+// npm start 
+// afterwards 
+// Watch Usage
+//  › Press a to run all tests.
+//  › Press f to run only failed tests.
+//  › Press q to quit watch mode.
+//  › Press p to filter by a filename regex pattern.
+//  › Press t to filter by a test name regex pattern.
+//  › Press Enter to trigger a test run.
+
+// sytnax to write test cases 
+// test('a string value that describes this test', () => { });
+
+// beforeAll();
+// beforeEach();
+// afterAll();
+// afterEach();
+
+// use this hook to render a component only once before all the test cases 
+beforeAll(() => {
+  console.log('beforeAll');
+});
+
+beforeEach(() => {
+  console.log('beforreEach');
+});
 
 // positive test case 
 test('render Data from Hello', () => {
@@ -27,6 +54,7 @@ test('render Data from EmpData', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
+
 // positive test case 
 test('render Data from SpringBootData', () => {
   render(
@@ -36,7 +64,6 @@ test('render Data from SpringBootData', () => {
   const linkElement = screen.getByText('Get All Employees');
   expect(linkElement).toBeInTheDocument();
 });
-
 
 // negative test case 
 test('render Data from SpringBootData', () => {
@@ -49,7 +76,6 @@ test('render Data from SpringBootData', () => {
 });
 
 
-// test('a string value that describes this test', () => { });
 
 // import { render, screen } from '@testing-library/react';
 // import App from './App';
